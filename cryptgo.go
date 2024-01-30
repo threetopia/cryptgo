25f5fe4a57a4d0a09c65b0083677c4ae49b9adc0
Tri Hartanto <tri@trihartanto.com>
2024-01-31 00:18:58 +0700

    chore: add license

--- cryptgo.go
+++ cryptgo.go
@@ -1,3 +1,5 @@
+// This is the package cryptgo
+// a simple crypt utils for Go
 package cryptgo
 
 import (
@@ -5,6 +7,26 @@ import (
 	"crypto/cipher"
 )
 
+// Encrypt returns a encrypted data in []byte and error.
+// The arguments consists with data []byte, secret string, and ivByte []byte.
+// The data argument is unecrypted data in []byte format.
+// The secret argument is an secret key in string with 24 char length.
+// The ivByte argument is an Initialization vector in string with 16 char length.
+//
+// Here is an example how to use Encrypt function:
+//
+//	func Encrypt() {
+//	    unecryptedStr := "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+//	    secret := "abcdefghij1234567890-+*/"
+//		iv := "1234567890abcdef"
+//		byteUnecryptedStr := []byte(unecryptedStr)
+//		encryptedData, err := Encrypt(byteStr, secret, []byte(iv))
+//		if err != nil {
+//			t.Errorf("error when encrypting text (%s)", err.Error())
+//			return
+//		}
+//		fmt.Println(string(encryptedData))
+//	}
 func Encrypt(data []byte, secret string, ivByte []byte) ([]byte, error) {
 	block, err := aes.NewCipher([]byte(secret))
 	if err != nil {
@@ -16,6 +38,25 @@ func Encrypt(data []byte, secret string, ivByte []byte) ([]byte, error) {
 	return encryptedData, nil
 }
 
+// Decrypt returns a decrypted data in []byte and error.
+// The arguments consists with data []byte, secret string, and ivByte []byte.
+// The data argument is ecrypted data in []byte format.
+// The secret argument is an secret key in string with 24 char length.
+// The ivByte argument is an Initialization vector in string with 16 char length.
+//
+// Here is an example how to use Decrypt function:
+//
+//	func Decrypt() {
+//	    ecryptedByte := []byte{29, 151, 234, 190, 13, 168, 36, 139, 157, 247, 141}
+//	    secret := "abcdefghij1234567890-+*/"
+//		iv := "1234567890abcdef"
+//		decryptedData, err := Decrypt(ecryptedByte, secret, []byte(iv))
+//		if err != nil {
+//			t.Errorf("error when encrypting text (%s)", err.Error())
+//			return
+//		}
+//		fmt.Println(string(decryptedData))
+//	}
 func Decrypt(encryptedData []byte, secret string, ivByte []byte) ([]byte, error) {
 	block, err := aes.NewCipher([]byte(secret))
 	if err != nil {
--- cryptgo_test.go
+++ cryptgo_test.go
@@ -5,12 +5,12 @@ import (
 	"testing"
 )
 
-var plainStr = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+var unecryptedStr = "Lorem ipsum"
 var secret = "abcdefghij1234567890-+*/"
 var iv = "1234567890abcdef"
 
 func TestEncryptDecryptStr(t *testing.T) {
-	byteStr := []byte(plainStr)
+	byteStr := []byte(unecryptedStr)
 	encryptData, err := Encrypt(byteStr, secret, []byte(iv))
 	if err != nil {
 		t.Errorf("error when encrypting text (%s)", err.Error())
@@ -20,7 +20,8 @@ func TestEncryptDecryptStr(t *testing.T) {
 		t.Errorf("error when encrypting text. somehow it output nothing")
 		return
 	}
-	decryptedData, err := Decrypt(encryptData, secret, []byte(iv))
+
+	decryptedData, err := Decrypt([]byte{29, 151, 234, 190, 13, 168, 36, 139, 157, 247, 141}, secret, []byte(iv))
 	if err != nil {
 		t.Errorf("error when encrypting text (%s)", err.Error())
 		return
